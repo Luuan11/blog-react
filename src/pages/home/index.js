175ce2acc88c5fec0cd294c5019644e4c08121a1
Luuan11 <luancraft55@gmail.com>
2024-02-08 22:58:19 -0300

    :sparkles: created new files and starting to make the posts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,7 @@
 import Banner from "components/Banner";
+import styles from "./home.module.css";
+
+import posts from "json/posts.json";
 
 export default function Home() {
     return (
@@ -6,7 +9,13 @@ export default function Home() {
 
             <Banner />
 
-            <h1>Olá</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => 
+                    <li key={post.id}> 
+                        Ler mais.
+                    </li>
+                )}
+            </ul>
 
         </main>
     )
